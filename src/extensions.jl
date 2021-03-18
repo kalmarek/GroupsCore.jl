879a4410696590811efc759957d5a6a8f5d76e53
Albin Ahlbäck <albin.ahlback@gmail.com>
2021-03-18 12:00:43 +0100

    Add docstring for some extensions

--- src/extensions.jl
+++ src/extensions.jl
@@ -12,6 +12,17 @@ function centralizer end
 function normalizer end
 function stabilizer end
 
+@doc Markdown.doc"""
+    index(subgroup::G, supgroup::G) where {G <: Group}
+
+Return the index $[H : G]$ for a subgroup $G \subseteq H$.
+"""
 function index end
+
+@doc Markdown.doc"""
+    left_coset_representatives(subgroup::G, supgroup::G) where {G <: Group}
+
+Return the representatives of the left coset $G H$ where $G \subseteq H$.
+"""
 function left_coset_representatives end
 function right_coset_representatives end
