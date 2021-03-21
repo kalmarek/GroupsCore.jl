4c7bc672f55c9e76513b19de808fc4edcfe6cb2e
Albin Ahlbäck <albin.ahlback@gmail.com>
2021-03-21 11:51:09 +0100

    Fix index and left_coset_representatives

--- src/extensions.jl
+++ src/extensions.jl
@@ -13,18 +13,18 @@ function normalizer end
 function stabilizer end
 
 @doc Markdown.doc"""
-    index(subgroup::G, group::G) where {G <: Group}
+    index(H::Gr, G::Gr) where {Gr <: Group}
 
-Return the index of the subgroup compared to the group. If subgroup is not
-contained in group, an error is thrown.
+Return the index $|G : H|$, where $H \subseteq G$. If $H$ is not contained
+in $G$, an error is thrown.
 """
 function index end
 
 @doc Markdown.doc"""
-    left_coset_representatives(subgroup::G, group::G) where {G <: Group}
+    left_coset_representatives(H::Gr, G::Gr) where {Gr <: Group}
 
-Return representatives of the left coset. If subgroup is not contained in
-group, an error is thrown.
+Return representatives of the left cosets $h G$ where $h$ are elements of $H$.
+If $H$ is not contained in $G$, an error is thrown.
 """
 function left_coset_representatives end
 function right_coset_representatives end
